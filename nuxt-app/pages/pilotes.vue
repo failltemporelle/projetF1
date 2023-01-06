42b2838f2f33af53202f98b4a1c4d5e60c9ad43e
failltemporelle <benjaminruyerpro@gmail.com>
2023-01-06 22:57:54 +0100

    update colors

--- nuxt-app/pages/pilotes.vue
+++ nuxt-app/pages/pilotes.vue
@@ -1,15 +1,14 @@
 <template>
   <navbar />
   <div class="flex flex-row flex-wrap place-content-center">
-    <div v-for="item in this.f1.points" class="card w-60 bg-base-100 shadow-xl ml-6 mt-6">
+    <div v-for="item in this.f1.points" class="card w-60 bg-red-700 shadow-xl ml-6 mt-6">
       <span class="indicator-item badge badge-primary text-teal-50 content-end">{{ item.points }} Points </span>
-      <h2 class="card-title text-center">{{ item.Driver.givenName }} {{ item.Driver.familyName }}</h2>
-
+      <h2 class="card-title">{{ item.Driver.givenName }} {{ item.Driver.familyName }}</h2>
       <figure><img
           :src="`https://raw.githubusercontent.com/failltemporelle/Projetp/main/f1-app/assets/pilotes/${item.Driver.driverId}.png`">
       </figure>
       <div class="card-body">
-        <progress class="progress w-50" :value=item.points :max="416"></progress>
+        <progress class="progress w-50" :value=item.points max="454"></progress>
       </div>
     </div>
   </div>
@@ -38,9 +37,6 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           this.f1.points =   data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-          this.f1.pointMax = data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].points;
-          console.log(this.f1.pointMax);
-          console.log(this.f1.points);
         });
     },
 
