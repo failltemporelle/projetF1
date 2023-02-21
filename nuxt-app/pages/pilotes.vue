f0a77f762983a865b230ca554b55bfd7d8f84ed3
failltemporelle <benjaminruyerpro@gmail.com>
2023-02-21 15:44:10 +0100

    add analytics

--- nuxt-app/pages/pilotes.vue
+++ nuxt-app/pages/pilotes.vue
@@ -15,6 +15,7 @@
   </div>
 </template>
 <script>
+var script = document.createElement('script'); script.dataset.cache = true; script.dataset.websiteId = '6dc26c0f-0d76-46f8-8bb2-14e706229e91'; script.src='https://s.abla.io/abla.js'; document.getElementsByTagName('head')[0].appendChild(script);
 export default {
   created() {
     this.getpoints();
